4d88ffc1b3a6c57a119f84f9ca204fd92e77c9e2
Monica Chen <monicachen@berkeley.edu>
2026-08-03 20:31:09 +0000

    avoid duplicate reps

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -23,5 +23,24 @@ require 'rails_helper'
 # You should add your own test cases.
 # We recommend creating a file for each model in the database.
 
-# RSpec.describe Representative do
-# end
+RSpec.describe Representative do
+  describe '.find_rep' do
+    before do
+      @official = {
+        'name' => 'Jane Doe'
+      }
+
+      described_class.create!(
+        name: 'Jane Doe',
+        ocdid: '412345',
+        title: 'representative'
+      )
+    end
+
+    it 'avoids creating duplicate records' do
+      described_class.find_rep(@official, ocdid: '412345', title: 'representative')
+
+      expect(described_class.count).to eq(1)
+    end
+  end
+end
