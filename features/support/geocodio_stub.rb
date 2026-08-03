d9d62d7f6531a9cbd6f30112716b6e9225f7ffc9
Max Yfantopoulos <maxyfantopoulos@berkeley.edu>
2026-08-03 01:23:20 +0000

    Point to correct fixture file in the stub

--- features/support/geocodio_stub.rb
+++ features/support/geocodio_stub.rb
@@ -4,7 +4,7 @@ WebMock.disable_net_connect!(allow_localhost: true)
 Before do
   stub_request(:post, /api\.geocod\.io/).to_return(
   status:  200,
-  body:    your_fixture_json,
+  body:    File.read(Rails.root.join('spec/fixtures/geocodio_response.json')),
   headers: { 'Content-Type' => 'application/json' }
   )
 end
