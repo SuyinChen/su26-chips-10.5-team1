ace66a31c18bb3cca26c878af915e8e4dfa2ee25
Max Yfantopoulos <maxyfantopoulos@berkeley.edu>
2026-08-04 04:18:14 +0000

    resolve robocop offenses

--- features/support/geocodio_stub.rb
+++ features/support/geocodio_stub.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'webmock/cucumber'
 WebMock.disable_net_connect!(allow_localhost: true)
 
 Before do
   stub_request(:post, /api\.geocod\.io/).to_return(
-  status:  200,
-  body:    File.read(Rails.root.join('spec/fixtures/geocodio_response.json')),
-  headers: { 'Content-Type' => 'application/json' }
+    status:  200,
+    body:    Rails.root.join('spec/fixtures/geocodio_response.json').read,
+    headers: { 'Content-Type' => 'application/json' }
   )
 end
