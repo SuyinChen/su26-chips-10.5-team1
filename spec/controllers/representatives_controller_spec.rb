1c859ba3c07c2281a8041813005470dfdc4e875d
Max Yfantopoulos <maxyfantopoulos@berkeley.edu>
2026-08-04 06:29:59 +0000

    [test] add controller spec for representatives shows action

---
+++ spec/controllers/representatives_controller_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe RepresentativesController do
+  describe 'GET #show' do
+    it 'assigns the requested representative' do
+      rep = Representative.create!(name: 'Max Yfantopoulos', party: 'Democrat')
+      get :show, params: { id: rep.id }
+      expect(assigns(:representative)).to eq(rep)
+    end
+  end
+end
