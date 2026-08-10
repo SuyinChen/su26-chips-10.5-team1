9af6860b49c4c13f595742da93b895d0a83d0c92
Johanna Liu <johanna.liu@berkeley.edu>
2026-08-10 00:25:12 +0000

    rspec test isn't working rn but here you go

--- spec/controllers/my_news_items_controller_spec.rb
+++ spec/controllers/my_news_items_controller_spec.rb
@@ -24,4 +24,27 @@ describe MyNewsItemsController do
       expect(response).to be_successful
     end
   end
+
+  describe 'POST create' do
+  it 'creates news item with an issue' do
+    expect {
+      post :create, params: {
+        representative_id: representative.id,
+        news_item: {
+          title: 'Test Article',
+          link: 'https://example.com',
+          description: 'description test',
+          representative_id: representative.id,
+          issue: 'Climate Change'
+        }
+      }
+    }.to change(NewsItem, :count).by(1)
+
+    expect(NewsItem.last.issue).to eq('Climate Change')
+  end
+end
+
+
+
+
 end
