6782b4da64ccde6b745ba4531630a8cd41e7aa2b
Max Yfantopoulos <maxyfantopoulos@berkeley.edu>
2026-08-11 21:34:55 -0500

    [feat] store and recompute average rating on news items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_08_12_013903) do
+ActiveRecord::Schema[7.2].define(version: 2026_08_12_021107) do
   create_table "counties", force: :cascade do |t|
     t.string "name", null: false
     t.integer "state_id", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_08_12_013903) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "issue"
+    t.decimal "average_rating", precision: 3, scale: 2
     t.index ["representative_id"], name: "index_news_items_on_representative_id"
   end
 
